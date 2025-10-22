041e39893430e714ac54315e92a855fa33da6715
Glebas4 <gleb.gurov.jr@gmail.com>
2025-10-22 21:53:08 +0300

    Create tf.py

---
+++ tf.py
@@ -0,0 +1,100 @@
+import cv2 as cv
+import numpy as np
+import rospy
+import tf2_ros
+import tf2_geometry_msgs
+from geometry_msgs.msg import PointStamped
+from clover import srv
+from std_srvs.srv import Trigger
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+from sensor_msgs.msg import Range
+from clover import long_callback
+from sensor_msgs.msg import CameraInfo
+import numpy as np
+
+
+
+get_telemetry = rospy.ServiceProxy('get_telemetry', srv.GetTelemetry)
+navigate = rospy.ServiceProxy('navigate', srv.Navigate)
+land = rospy.ServiceProxy('land', Trigger)
+
+#pub = rospy.Publisher('buildings', String, queue_size=1)
+tf_buffer = tf2_ros.Buffer()
+listener = tf2_ros.TransformListener(tf_buffer)
+bridge = CvBridge()
+K, D = None
+
+colors = {
+    "red"   : ((0, 0, 220),(50, 50, 255)),
+    "green" : ((0, 220, 0),(50, 255, 50)),
+    "blue"  : ((255, 0, 0),(255, 60, 60)),
+    "yellow": ((0, 220, 220),(0, 255, 255)) 
+}
+buildings = []
+
+
+def range_callback(msg):
+    global Z
+    Z = msg.range
+
+
+def cam_info():
+    global K, D
+    msg = rospy.wait_for_message('/main_camera/camera_info', CameraInfo, timeout=1) # Информация о камере
+    K = np.array(msg.K).reshape(3, 3) #Создание матрицы 3x3
+    D = np.array(msg.D) #Коэффициенты дисторсии
+    return K, D
+
+
+def scan(img):
+    detects = []
+    for col, val in colors.items():
+        bin = cv.inRange(img, val[0], val[1])
+        count = cv.countNonZero(bin)
+        if count > 30:
+            detects.append((col, bin))
+        
+    return detects
+
+
+def get_centers(bin_img):
+    contours, _ = cv.findContours(bin_img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    cords = []
+    for cnt in contours:
+        M = cv.moments(cnt)
+        if M["m00"] != 0:
+            x = int(M["m10"] / M["m00"])
+            y = int(M["m01"] / M["m00"])
+            cords = np.array([[[x, y]]], dtype=np.float32)
+    return cords
+        
+
+
+@long_callback
+def image_callback(data):
+    img = bridge.imgmsg_to_cv2(data, 'bgr8')
+    detects = scan(img)
+    if detects:
+        for pnt in detects:
+            cords = get_centers(pnt[1])
+            cords_norm = cv.undistortPoints(cords, K, D, P=None)
+            x = float(cords_norm[0,0,0])
+            y = float(cords_norm[0,0,1])
+            r_cam = np.array([x, y, 1.0])
+            P_cam = r_cam * Z # Точка в координатах камеры
+            print(pnt[0], P_cam)
+
+
+def main():
+    navigate(x=0, y=0, z=2.5, frame_id="body", auto_arm=True)
+    rospy.sleep(5)
+
+   
+if __name__ == '__main__':
+    rospy.init_node('flight')
+    rospy.Subscriber('rangefinder/range', Range, range_callback)
+    image_sub = rospy.Subscriber('main_camera/image_raw', Image, image_callback)
+    K, D = cam_info()
+    main()
+    rospy.spin()
