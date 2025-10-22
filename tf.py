e06e6599ddf443750c73a064c262ba64c59dc0e2
Glebas4 <gleb.gurov.jr@gmail.com>
2025-10-22 22:25:40 +0300

    Update tf.py

--- tf.py
+++ tf.py
@@ -16,8 +16,6 @@ import numpy as np
 
 
 
-rospy.init_node('flight')
-
 get_telemetry = rospy.ServiceProxy('get_telemetry', srv.GetTelemetry)
 navigate = rospy.ServiceProxy('navigate', srv.Navigate)
 land = rospy.ServiceProxy('land', Trigger)
@@ -107,6 +105,7 @@ def main():
 
    
 if __name__ == '__main__':
+    rospy.init_node('flight')
     rospy.Subscriber('rangefinder/range', Range, range_callback)
     image_sub = rospy.Subscriber('main_camera/image_raw', Image, image_callback)
     K, D = cam_info()
